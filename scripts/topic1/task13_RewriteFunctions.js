a6f2548373a0c26cd9900402f4d0954c2470eafc
TrueInTwoyes <ti2pho@gmail.com>
2022-03-21 17:47:42 +0700

    complete 13 task

---
+++ scripts/topic1/task13_RewriteFunctions.js
@@ -0,0 +1,9 @@
+function checkAgeTern(age){
+    age = prompt("Input your age", "");
+    return (age > 18) ? true : confirm("Do you have parent access?"); 
+}
+
+function checkAgeLogic(){
+    number = prompt("Input your age", "");
+    return (age > 18) || confirm("Do you have parent access?");
+}

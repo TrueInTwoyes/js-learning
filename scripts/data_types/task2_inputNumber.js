ef3be402b83c727fb77df537b813194ec59bf693
TrueInTwoyes <ti2pho@gmail.com>
2022-04-06 16:02:20 +0700

    complete task 2 in data types topic

---
+++ scripts/data_types/task2_inputNumber.js
@@ -0,0 +1,16 @@
+function readNumber() {
+    
+    let value;
+
+    while (true) {
+        
+        value = +prompt("Input number:", 0);
+        
+        if (typeof(value) === 'number' && !isNaN(value)) {
+            alert(value);
+            break;
+        } else if (!value) {
+            break;
+        } 
+    }
+}
